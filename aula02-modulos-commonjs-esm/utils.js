851b63443c2a6b4b65acd8a2430230574ef04a05
Alessandra_minucci <alessandraminucci05@gmail.com>
2026-09-11 16:49:36 -0300

    chore: configura projeto para usar módulos ES

---
+++ aula02-modulos-commonjs-esm/utils.js
@@ -0,0 +1,9 @@
+export function formatLog(mensagem){
+
+    const dataAtual = new Date().toISOString().split('T')[0];
+
+    const horaAtual = new Date().toLocaleDateString();
+
+    return `[${dataAtual} ${horaAtual}] - ${mensagem}`;
+
+}
