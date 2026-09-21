a6b96461bee22776d3baf69bf6a4298d46a6c018
Alessandra_minucci <alessandraminucci05gmail.com>
2026-09-21 16:59:31 -0300

    feat: adiciona import de ConvidadosService

--- aula08-09-metodo-get-post-put-patch-delete/src/app.module.ts
+++ aula08-09-metodo-get-post-put-patch-delete/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-import { ConviddadosController } from './convidados.controller.js';
+import { ConvidadosController } from './convidados.controller.js';
+import { ConvidadosService } from './convidados.service.js';
 
 @Module({
   imports: [],
-  controllers: [AppController, ConviddadosController],
-  providers: [AppService],
+  controllers: [AppController, ConvidadosController],
+  providers: [AppService, ConvidadosService],
 })
 export class AppModule {}
